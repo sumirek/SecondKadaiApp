c9622462156aa6bb8f5d3face9e753d7a8669ff0
sumire.kuroda <sumire.a.kuroda@fujifilm.com>
2024-10-21 14:09:52 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by UI3 on 2024/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
